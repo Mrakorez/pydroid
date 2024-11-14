a590b5401c6bf97394474332e96de7f7a573a42e
Mrakorez <deadline21th@gmail.com>
2024-11-14 14:55:09 +0200

    refactor: Create symlinks in `customize.sh`
    - Ensure compatibility by avoiding symlinks in ZIP archives.

--- customize.sh
+++ customize.sh
@@ -1,5 +1,16 @@
 # shellcheck shell=ash
 
+link_files() {
+  local target_dir="${MODPATH}/system/bin"
+
+  cd "${target_dir}" ||
+    abort "! Cannot cd to ${target_dir}"
+
+  ln -s pydoc3.13 pydoc3
+  ln -s python3.13-config python3-config
+  ln -s python3.13 python3
+}
+
 case "${ARCH}" in
   "arm64")
     tar -xf "${MODPATH}/python/arm64.tar.xz" -C "${MODPATH}" \
@@ -20,6 +31,8 @@ tar -xf "${MODPATH}/python/stdlib.tar.xz" -C "${MODPATH}" \
 set_perm_recursive "${MODPATH}/system/bin" 0 0 0755 0744
 set_perm_recursive "${MODPATH}/usr/bin" 0 0 0755 0744
 set_perm_recursive "${MODPATH}/usr/lib" 0 0 0755 0644
+
+link_files
 
 mkdir "${MODPATH}/usr/tmp"
 
